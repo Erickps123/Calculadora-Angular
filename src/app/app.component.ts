18423e4be6e51684446b302b6a8cd9e2f1dae2ed
Erickps123 <erickkpsanchez@gmail.com>
2020-03-12 20:28:05 -0400

    v0.0.0.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'Calculadora';
+  public number1 : number;
+  public number2 : number;
+  public result : number;
+  public op: number;
+
+  public Resolver(){
+    if(this.op==1){
+      this.result = this.number1 + this.number2
+    }else if(this.op==2){
+      this.result = this.number1 - this.number2
+    }else if(this.op==3){
+      this.result = this.number1 * this.number2
+    }else if(this.op==4){
+      this.result = this.number1 / this.number2
+    }else if(this.op==5){
+      this.result = Math.pow(this.number1, this.number2)
+    }else if(this.op==6){
+      this.result = Math.sqrt(this.number1)
+    }else if(this.op==7){
+      this.result = Math.log(this.number1)
+    }else if(this.op==8){
+      this.result = Math.log10(this.number1)
+    }
+  }
 }
